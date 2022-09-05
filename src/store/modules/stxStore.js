b9540fbc121d33016f41cba0526f79e2c1f2a2a0
yhist <yhyh0903@gmail.com>
2022-09-05 13:06:27 +0900

    json 경로 변경

--- src/store/modules/stxStore.js
+++ src/store/modules/stxStore.js
@@ -10,7 +10,7 @@ const actions = {
   // gnb.json 데이터를 axios로 받아온다.
   // mutation: 즉, state를 업데이트 하기 위해 필요한 메서드(commit)
   fetchGnb({commit}){
-    axios.get('/data/gnb.json')
+    axios.get('./data/gnb.json')
     .then(response => {
       // console.log("axios", response);
       // commit('mutation 메서드 이름', 전달할 값);
@@ -20,7 +20,7 @@ const actions = {
   },
   fetchNotice({commit}){
     // new.json을 axios로 호출한다.
-    axios.get('/data/notice.json')
+    axios.get('./data/notice.json')
     .then(response => {
       // console.log(response)
       // mutation으로 자료를 전송한다. commit 필요
@@ -31,7 +31,7 @@ const actions = {
     .catch(err => console.log(err))
   },
   fetchNews({commit}){
-    axios.get('/data/news.json')
+    axios.get('./data/news.json')
     .then(response => {
       // console.log(response)
       commit('UPDATE_NEWS', response.data)
